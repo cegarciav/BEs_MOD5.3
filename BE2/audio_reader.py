17cb8b1dabb029100aadf8b6ce2371051bae862a
melkarmo <moncefelkarmoudi@outlook.fr>
2020-01-07 20:12:54 +0100

    code structuring

--- BE2/audio_reader.py
+++ BE2/audio_reader.py
@@ -9,12 +9,14 @@ order = 10
 omega_v = 1
 omega_d = 1
 omega_h = 1
+k = 3
+sample_size = 20 # 2000 maximum (déconseillé sur pc)
 
 """Fe, s1 = wav.read(files_folder + "0_jackson_0.wav")
 _, s2 = wav.read(files_folder + "0_jackson_5.wav")
 window_semi_size = int(Fe * duree_trame / 2)"""
 
-def get_r_vector(order, window):
+def get_r_vector(window):
     r_vector = []
     for index in range(order + 1):
         vect_1 = window[:len(window) - index]
@@ -38,20 +40,20 @@ def get_a_vector(matrix):
     a_vector = mult / mult[0]
     return a_vector[1:]
 
-def lpc_coeffs(order, window):
-    r_vector = get_r_vector(order, window)
+def lpc_coeffs(window):
+    r_vector = get_r_vector(window)
     r_matrix = get_r_matrix(r_vector)
     a_vector = get_a_vector(r_matrix)
     return a_vector
 
-def lpc_coeffs_list(signal, window_semi_size, order):
+def lpc_coeffs_list(signal, window_semi_size):
     s_length = len(signal)
     window_size = window_semi_size * 2
     start = 0
     lpc_list = []
     while True:
         window = signal[start : start + window_size]
-        lpc_list.append(lpc_coeffs(order, window))
+        lpc_list.append(lpc_coeffs(window))
         start += window_semi_size
         if start + window_size > s_length:
             break
@@ -60,7 +62,7 @@ def lpc_coeffs_list(signal, window_semi_size, order):
 def euclidean_distance(vect1, vect2):
     return np.sqrt(sum((vect1 - vect2) ** 2))
 
-def distance_elastique(coeffs_1, coeffs_2, omega_v, omega_d, omega_h):
+def distance_elastique(coeffs_1, coeffs_2):
     distance_matrix = np.zeros((len(coeffs_1), len(coeffs_2)))
     # on calcule la première distance (0,0)
     distance_matrix[0,0] = euclidean_distance(coeffs_1[0], coeffs_2[0])
@@ -80,15 +82,15 @@ def distance_elastique(coeffs_1, coeffs_2, omega_v, omega_d, omega_h):
     # on retourne la distance optimale
     return distance_matrix[-1,-1] / (len(coeffs_1) + len(coeffs_2))
 
-def distance_entre_signaux(s1, s2, window_semi_size, order, omega_v, omega_d, omega_h):
-    coeffs_1 = lpc_coeffs_list(s1, window_semi_size, order)
-    coeffs_2 = lpc_coeffs_list(s2, window_semi_size, order)
-    return distance_elastique(coeffs_1, coeffs_2, omega_v, omega_d, omega_h)
+def distance_entre_signaux(s1, s2, window_semi_size):
+    coeffs_1 = lpc_coeffs_list(s1, window_semi_size)
+    coeffs_2 = lpc_coeffs_list(s2, window_semi_size)
+    return distance_elastique(coeffs_1, coeffs_2)
 
 def chargement_donnees():
     file_names = os.listdir(files_folder)
     shuffle(file_names)
-    file_names = file_names[:taille_lot]
+    file_names = file_names[:sample_size]
     X = [ files_folder + name for name in file_names ]
     Y = [ int(name[0]) for name in file_names ]
     delimitation = int(0.8*len(X))
@@ -107,7 +109,7 @@ def kppv_distances(Xtest, Xapp):
         for j in range(len(Xapp)):
             _, s2 = wav.read(Xapp[j])
             if (len(s2.shape) > 1) : s2 = s2[:,0]
-            Dist[i,j] = distance_entre_signaux(s1, s2, window_semi_size, order, omega_v, omega_d, omega_h)
+            Dist[i,j] = distance_entre_signaux(s1, s2, window_semi_size)
     return Dist
 
 def kppv_predict(Dist, Yapp, K):
@@ -126,8 +128,6 @@ def accuracy(Xapp, Yapp, Xtest, Ytest, K):
     Ypred = kppv_predict(Dist, Yapp, K)
     print('Accuracy = {} !'.format(evaluation_classifieur(Ytest, Ypred)))
 
-k = 3
-taille_lot = 10 # 2000 maximum (déconseillé sur pc)
 Xapp, Yapp, Xtest, Ytest = chargement_donnees()
 accuracy(Xapp, Yapp, Xtest, Ytest, k)
             
